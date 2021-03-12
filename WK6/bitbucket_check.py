3af5db5bfe32a5bf421455dc142eab9e7b4dc56a
BenjaminTan1 <78334905+BenjaminTan1@users.noreply.github.com>
2021-03-12 12:05:05 -0600

    Delete WK6 directory

--- WK6/bitbucket_check.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.get(url="https://api.bitbucket.org/2.0/repositories")
-
-# let's look at the response code
-
-print(response.status_code)
-print(response.json())
